f90dc7008d1b1bd1d34099472b6d49171ddc4cc7
Juan Gracia <juangrazz@hotmail.com>
2021-05-16 16:16:33 +0200

    Fix

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require("cors");
 const config = require('./keys.js');
-const path = require('path');
 
 const app = express();
 
