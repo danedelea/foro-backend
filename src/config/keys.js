43c55ab4fa7580b41e89910a19b55829fbc789b8
Juan Gracia <juangrazz@hotmail.com>
2021-05-30 09:51:41 +0200

    Heroku config

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 4000,
-    SERVER: "http://localhost:4200",
+    SERVER: "*",
     SECRET_KEY_TOKEN: "7MLicZ?5(;v&(Rd|8kCA",
     SECRET_KEY_CRYPTO: "vOVH6sdmpNWjRRIqCc7rdxs01lwHzfr3",
     ALGORITH: "aes-256-cbc",
--- src/database.js
+++ src/database.js
@@ -2,10 +2,10 @@ const mysql = require("mysql");
 
 const pool = mysql.createPool({
     connectionLimit: 10,
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'tinder_unizar',
+    host: 'us-cdbr-east-04.cleardb.com',
+    user: 'b1bf824e91c6e4',
+    password: '776b378d',
+    database: 'heroku_bb94b7f3e3fef39',
     charset: "utf8mb4"
 });
 
